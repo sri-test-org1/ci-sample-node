8982d401062aed2e27c2fc039da4b37545ae3d8f
Srikanth Hanagud <shanagud@contractor.linuxfoundation.org>
2021-03-26 00:01:39 +0530

    Initial node.js work for ci testing
    Signed-off-by: Srikanth Hanagud <shanagud@contractor.linuxfoundation.org>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ci-sample-node
 ci sample for testing nodejs projects
+
+Reference - https://github.com/webrolls/hello-world-node-package/blob/master/README.md
+
---
+++ index.js
@@ -0,0 +1,12 @@
+function helloWorld() {
+	console.log('Hello World!');
+}
+
+function doSomethingAwesome() {
+	console.log('Doing something awesome...');	
+}
+
+module.exports = {
+	helloWorld: helloWorld,
+	doSomethingAwesome: doSomethingAwesome
+}
